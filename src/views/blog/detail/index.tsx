921c7025a2233d6bd829b99b9566d5b04d7c25da
王俐 <>
2024-12-18 14:03:08 +0800

    feat:detail loading

--- src/views/blog/detail/index.tsx
+++ src/views/blog/detail/index.tsx
@@ -15,7 +15,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-
+import Loading from "@/assets/images/loading.svg?react";
 import "./detail.scss";
 interface Title {
   title: string;
@@ -65,100 +65,102 @@ export const Detail: React.FC = () => {
     const index = levelList.indexOf(level);
     return index * 15;
   };
-  return (
-    blog && (
-      <div className="mx-auto blog_detail py-[50px]">
-        <div
-          className="left w-[75%] h-full overflow-auto max-md:w-full"
-          data-aos="fade-up"
-        >
-          <div className="tags flex items-center gap-3 mb-7">
-            {blog.tags.map((tag) => (
-              <Tag key={tag.id}>{tag.name}</Tag>
-            ))}
-          </div>
-          <p className="title font-bold text-4xl leading-10 mb-7 max-md:text-2xl">
-            {blog.title}
-          </p>
-          <p className="description font-bold text-xl leading-7 mb-7 max-md:text-[18px]">
-            {blog.description}
-          </p>
-          <div className="time flex items-center gap-3 mb-7">
-            <Icon
-              icon="material-symbols:alarm-outline"
-              className="text-theme-primary"
-            />
-            <p className="mb-0">{formatDate(blog.createdAt)}</p>
-          </div>
-          <div className="thumbnail mb-5">
-            <img
-              data-aos="fade-up"
-              src={blog.thumbnail}
-              alt=""
-              className="w-full h-auto rounded-xl aspect-[5/2] object-cover"
-            />
-          </div>
-          <div
-            className="content prose prose-lg max-w-none dark:prose-invert prose-pre:p-0"
-            ref={contentRef}
-          >
-            <ReactMarkdown
-              remarkPlugins={[remarkGfm]}
-              rehypePlugins={[rehypeRaw]}
-              components={{
-                code({
-                  inline,
-                  className,
-                  children,
-                  ...props
-                }: ComponentPropsWithoutRef<"code"> & { inline?: boolean }) {
-                  const match = /language-(\w+)/.exec(className || "");
-                  return !inline && match ? (
-                    <SyntaxHighlighter
-                      // @ts-expect-error style type mismatch
-                      style={{ ...vscDarkPlus, fontSize: "30px" }}
-                      language={match[1]}
-                      PreTag="div"
-                      {...props}
-                    >
-                      {String(children).replace(/\n$/, "")}
-                    </SyntaxHighlighter>
-                  ) : (
-                    <code className={className} {...props}>
-                      {children}
-                    </code>
-                  );
-                },
-              }}
-            >
-              {blog.content}
-            </ReactMarkdown>
-          </div>
+  return blog ? (
+    <div className="mx-auto blog_detail py-[50px]">
+      <div
+        className="left w-[75%] h-full overflow-auto max-md:w-full"
+        data-aos="fade-up"
+      >
+        <div className="tags flex items-center gap-3 mb-7">
+          {blog.tags.map((tag) => (
+            <Tag key={tag.id}>{tag.name}</Tag>
+          ))}
         </div>
-        <div className="right border-l border-gray-200 pl-5 max-md:hidden">
-          <h2 className="text-2xl font-bold mb-4">Anchor</h2>
-          <ul className="space-y-2" data-aos="fade-up">
-            {titles.map((title, index) => (
-              <li
-                key={index}
-                className="list-none leading-7 text-base cursor-pointer hover:bg-theme-secondary px-2 py-1 rounded-md"
-                style={{
-                  marginLeft: getPaddingLeft(title.level),
-                }}
-                onClick={() => {
-                  title.node.scrollIntoView({
-                    behavior: "smooth",
-                    block: "start",
-                  });
-                }}
-              >
-                {title.title}
-              </li>
-            ))}
-          </ul>
+        <p className="title font-bold text-4xl leading-10 mb-7 max-md:text-2xl">
+          {blog.title}
+        </p>
+        <p className="description font-bold text-xl leading-7 mb-7 max-md:text-[18px]">
+          {blog.description}
+        </p>
+        <div className="time flex items-center gap-3 mb-7">
+          <Icon
+            icon="material-symbols:alarm-outline"
+            className="text-theme-primary"
+          />
+          <p className="mb-0">{formatDate(blog.createdAt)}</p>
+        </div>
+        <div className="thumbnail mb-5">
+          <img
+            data-aos="fade-up"
+            src={blog.thumbnail}
+            alt=""
+            className="w-full h-auto rounded-xl aspect-[5/2] object-cover"
+          />
+        </div>
+        <div
+          className="content prose prose-lg max-w-none dark:prose-invert prose-pre:p-0"
+          ref={contentRef}
+        >
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+            components={{
+              code({
+                inline,
+                className,
+                children,
+                ...props
+              }: ComponentPropsWithoutRef<"code"> & { inline?: boolean }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    // @ts-expect-error style type mismatch
+                    style={{ ...vscDarkPlus, fontSize: "30px" }}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          >
+            {blog.content}
+          </ReactMarkdown>
         </div>
       </div>
-    )
+      <div className="right border-l border-gray-200 pl-5 max-md:hidden">
+        <h2 className="text-2xl font-bold mb-4">Anchor</h2>
+        <ul className="space-y-2" data-aos="fade-up">
+          {titles.map((title, index) => (
+            <li
+              key={index}
+              className="list-none leading-7 text-base cursor-pointer hover:bg-theme-secondary px-2 py-1 rounded-md"
+              style={{
+                marginLeft: getPaddingLeft(title.level),
+              }}
+              onClick={() => {
+                title.node.scrollIntoView({
+                  behavior: "smooth",
+                  block: "start",
+                });
+              }}
+            >
+              {title.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  ) : (
+    <div className="w-full flex items-center justify-center h-screen">
+      <Loading />
+    </div>
   );
 };
 export default Detail;
