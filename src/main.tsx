a3135f3507792adff72faf70f8bec947d5c3b643
王俐 <>
2025-04-11 17:02:20 +0800

    feat:增加消息推送

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App";
 // 导入 antd 样式
 import "antd/dist/reset.css";
 import "./style/index.scss";
+import { init } from "./utils/serviceWorker";
+
+init();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
